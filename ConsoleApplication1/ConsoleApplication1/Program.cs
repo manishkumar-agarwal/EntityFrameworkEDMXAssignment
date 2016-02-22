63ab7d46d4d4588e9deec408aa6f4679f6d7a990
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2016-02-21 22:17:13 -0800

    Implemented functionality to generate the edmx file from the data model and also wrote simple code tod isplay all the employees in the database

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationusingEDMX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!! Welcome to the Telephone System");
+
+            using(var telepehoneSystemDBContext = new TelephoneBillingSystemContext())
+            {
+                var listOfEmployees = telepehoneSystemDBContext.Employees.ToList();
+
+                foreach(var employee in listOfEmployees)
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+        }
+    }
+}
